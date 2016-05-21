bcf8e82aecbe10935f56573cf303235723948b95
Andrew Jeffery <andrew@aj.id.au>
2016-05-21 16:49:21 +0930

    WIP: Make psave understand impact on current month
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- lib/fpos/psave.py
+++ lib/fpos/psave.py
@@ -25,7 +25,9 @@ from pprint import pprint
 import sys
 import datetime
 import monthdelta
+from itertools import chain
 
+mtt = namedtuple("mtt", [ "month", "transactions" ])
 tt = namedtuple("tt", [ "month", "income", "expenses" ])
 cdt = namedtuple("cdt", [ "name", "period", "last", "mean" ])
 
@@ -36,16 +38,17 @@ def to_month_groups(transactions):
     monthly_grouper = PeriodGroup(extract_month)
     monthly_grouper.add_all(transactions)
     monthly_transactions, = monthly_grouper.groups()
-    return monthly_transactions
+    mtl = [ mtt(month, mts) for month, mts in monthly_transactions.items() ]
+    return sorted(mtl, key=lambda x: pm(x.month))
 
 def to_month_tts(transactions):
     months = [] 
-    for month, mts in transactions.items():
-        income = sum(float(elem[1]) for elem in mts
+    for mts in transactions:
+        income = sum(float(elem[1]) for elem in mts.transactions
                 if float(elem[1]) >= 0)
-        expenses = sum(float(elem[1]) for elem in mts
+        expenses = sum(float(elem[1]) for elem in mts.transactions
                 if float(elem[1]) < 0)
-        mtt = tt(month, income, expenses)
+        mtt = tt(mts.month, income, expenses)
         months.append(mtt)
     return months
 
@@ -59,12 +62,12 @@ def to_cycle_descriptors(groups):
         cyclic_descriptors.append(cdt(name, period, last, mean))
     return cyclic_descriptors
 
-def balance(tts, longest):
-    assert longest < len(tts)
-    ltts = len(tts)
-    margins = [ elem.income + elem.expenses for elem in tts ]
-    for i in range(ltts - longest, ltts):
+def balance(margins, longest):
+    nmonths = len(margins)
+    assert longest < nmonths, "longest = {}, nmonths = {}".format(longest, nmonths)
+    for i in range(nmonths - longest, nmonths):
         if margins[i] < 0:
+            # Find savings, i.e. backwards in time
             for j in range(i, -1, -1):
                 if margins[j] > 0:
                     v = min(-margins[i], margins[j])
@@ -75,39 +78,61 @@ def balance(tts, longest):
     return margins
 
 def psave(transactions):
-    groups = cdesc(t for t in transactions if t[3] not in blacklist)
-    last = pd(transactions[-1][0])
-    cyclic_groups = list(prune_groups(groups, last))
-    acyclic_transactions = [ elem for elem in transactions
-            if elem[3] != "Internal" ]
-    monthly_transactions = to_month_groups(acyclic_transactions)
-    monthly_tts = sorted(to_month_tts(monthly_transactions),
-            key=lambda x: pm(x.month))
-    pprint(monthly_tts)
+    external = [ elem for elem in transactions if elem[2] != "Internal" ]
+    monthly_transactions = to_month_groups(external)
+    completed = list(chain(*[mts.transactions for mts in monthly_transactions[:-1]]))
+    incomplete = monthly_transactions[-1].transactions
+    assert 3 < len(completed)
+    groups = cdesc(t for t in completed if t[2] not in blacklist)
+    last_completed = pd(completed[-1][0])
+    cyclic_groups = list(prune_groups(groups, last_completed))
+    monthly_tts = sorted(to_month_tts(monthly_transactions[:-1]), key=lambda x: pm(x.month))
     cyclic_descriptors = to_cycle_descriptors(cyclic_groups)
     sorted_cyclic_descriptors = \
             sorted(cyclic_descriptors, key=lambda x: x.period, reverse=True)
-    margins = balance(monthly_tts, int(sorted_cyclic_descriptors[0].period / 31))
-    # Expenses is negative
-    pprint(margins)
+    unbalanced = [ elem.income + elem.expenses for elem in monthly_tts ]
+    balanced = balance(unbalanced, int(sorted_cyclic_descriptors[0].period / 31))
+    # Expenses are negative
     targets = { cd : cd.mean / (cd.period / 31) for cd in cyclic_descriptors }
     actuals = { cd : 0 for cd in sorted_cyclic_descriptors }
     for cd in sorted_cyclic_descriptors:
-        print(cd.name)
-        md = monthdelta.monthmod(cd.last, last)[0].months
-        print(md)
-        if md == 0 and margins[-1] > 0:
-            v = min(-(cd.mean * (31 / cd.period)), margins[-1])
-            margins[-1] -= v
+        md = monthdelta.monthmod(cd.last, last_completed)[0].months
+        if md == 0 and balanced[-1] > 0:
+            v = min(-(cd.mean * (31 / cd.period)), balanced[-1])
+            balanced[-1] -= v
             actuals[cd] += v
         else:
             for month in range(-md, 0):
-                print("month index: {}".format(month))
-                if margins[month] > 0:
-                    v = min(-(cd.mean / (cd.period / 31)), margins[month])
-                    print(v)
-                    margins[month] -= v
+                if balanced[month] > 0:
+                    v = min(-(cd.mean / (cd.period / 31)), balanced[month])
+                    balanced[month] -= v
                     actuals[cd] += v
+
+    due = {}
+    current = pd(transactions[-1][0])
+    for cd in sorted_cyclic_descriptors:
+        when = cd.last + datetime.timedelta(cd.period)
+        if when.month == current.month and when.year == current.year:
+            due[cd.name] = cd
+    pprint(due)
+    print()
+
+    for transaction in incomplete:
+        if transaction[3] in blacklist:
+            continue
+        # FIXME: Use nlcs() to test membership
+        desc = transaction[2]
+        if desc in due:
+            print("Found {} in due".format(desc))
+            spent = float(transaction[1])
+            saved = actuals[due[desc]]
+            effective = min(0, spent + saved)
+            remaining = max(0, spent + saved)
+            print("{} saved {}, spent {}, effective {}, impact {}"
+                    .format(desc, saved, spent, effective, remaining))
+            print()
+            actuals[due[desc]] = remaining
+
     print("TARGETS")
     print("-------")
     #pprint({ (k.name, k.period) : v for k, v in targets.items() if v != 0 })
@@ -120,7 +145,7 @@ def psave(transactions):
     print()
     print("MARGINS")
     print("-------")
-    pprint(margins)
+    pprint(balanced)
 
 if __name__ == "__main__":
     transactions = [ row for row in csv.reader(sys.stdin) ]
